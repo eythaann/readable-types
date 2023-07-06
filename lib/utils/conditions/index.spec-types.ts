08f1c1674e43bac8323b4098a884ceb129515bce
eythaann <eythan.cvt@gmail.com>
2023-07-06 17:59:27 -0500

    feature: Add a better way to write complex conditions types

--- lib/utils/conditions/index.spec-types.ts
+++ lib/utils/conditions/index.spec-types.ts
@@ -18,4 +18,14 @@ describeType('If', () => {
     assertType<If<boolean, '1234', 1234>>().equals<'1234' | 1234>(),
     assertType<If<boolean, any, unknown>>().toBeAny(), // in this case any | unknown resolve it as any
   ]);
+
+  testType('Should use condition object (variant 1)', [
+    assertType<If<{condition: true, type: string, elseType: number}>>().equals<string>(),
+    assertType<If<{condition: false, type: string, elseType: number}>>().equals<number>(),
+  ]);
+
+  testType('Should use condition object (variant 2)', [
+    assertType<If<{condition: true, trueType: string, falseType: number}>>().equals<string>(),
+    assertType<If<{condition: false, trueType: string, falseType: number}>>().equals<number>(),
+  ]);
 });
--- lib/utils/conditions/index.ts
+++ lib/utils/conditions/index.ts
@@ -1,16 +1,44 @@
-export type Else<T> = T;
+
+type Condition1 = { 
+  condition: boolean;
+  type: unknown;
+  elseType: unknown;
+}
+
+type Condition2 = {
+  condition: boolean;
+  trueType: unknown;
+  falseType: unknown;
+}
+
 
 /**
- * Conditional type that selects one of two possible types based on a boolean condition.
+ * Conditional type that selects one of two possible types based on a boolean condition or a condition object.
  *
  * @example
  * ```
+ * // using boolean condition
  * type A = If<true, string, number>;
  * //   ^ Type A = string
  * type B = If<false, string, number>;
  * //   ^ Type B = number
  * type C = If<boolean, string, number>;
- * //   ^ Type c = string | number
+ * //   ^ Type C = string | number
+ *
+ * // using condition object (variant 1)
+ * type D = If<{condition: true, type: string, elseType: number}>;
+ * //   ^ Type D = string
+ * type E = If<{condition: false, type: string, elseType: number}>;
+ * //   ^ Type E = number
+ *
+ * // using condition object (variant 2)
+ * type F = If<{condition: true, trueType: string, falseType: number}>;
+ * //   ^ Type F = string
+ * type G = If<{condition: false, trueType: string, falseType: number}>;
+ * //   ^ Type G = number
  * ```
  */
-export type If<Condition extends boolean, TrueCase, FalseCase = never> = Condition extends true ? TrueCase : FalseCase;
+export type If<Condition extends boolean | Condition1 | Condition2, TrueCase = never, FalseCase = never> = 
+Condition extends boolean ? Condition extends true ? TrueCase : FalseCase
+: Condition extends Condition1 ? Condition['condition'] extends true ? Condition['type'] : Condition['elseType']
+: Condition extends Condition2 ? Condition['condition'] extends true ? Condition['trueType'] : Condition['falseType'] : never
