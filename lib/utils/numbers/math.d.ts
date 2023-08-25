e244381cf5afcbe877f54093d96af24c2150b595
eythaann <eythan.cvt@gmail.com>
2023-08-25 07:30:58 -0500

    enh: Preparation to 2.0

--- lib/utils/numbers/math.d.ts
+++ lib/utils/numbers/math.d.ts
@@ -2,30 +2,21 @@ import { Addition, Substraction } from './_mathDecimal';
 
 /**
  * The `Add` type takes two numeric literal types and produces their sum as a string literal type.
- *
- * @template A Numeric literal type to be added.
- * @template B Numeric literal type to be added.
- *
  * @example
  * type Result = Add<5, 10>;
- * //   ^? Result = "15"
+ * //   ^? Result = 15
  *
- * type AnotherResult = Add<2, 3>;
- * //   ^? AnotherResult = "5"
+ * type Result = Add<2, 3>;
+ * //   ^? Result = 5
  */
 export type Add<A extends string | number, B extends string | number> = Addition.Add<A, B>;
 
 /**
- * The `Add` type takes two numeric literal types and produces their sum as a string literal type.
- *
- * @template A Numeric literal type to be added.
- * @template B Numeric literal type to be added.
- *
+ * The `Add` type takes two numeric literal types and produces their substraction as a string literal type.
  * @example
- * type Result = Add<5, 10>;
- * //   ^? Result = "15"
- *
- * type AnotherResult = Add<2, 3>;
- * //   ^? AnotherResult = "5"
+ * type Result = Add<10, 5>;
+ * //   ^? Result = 5
+ * type Result = Add<2, 3>;
+ * //   ^? Result = -1
  */
 export type Substract<A extends string | number, B extends string | number> = Substraction.Substract<A, B>;
