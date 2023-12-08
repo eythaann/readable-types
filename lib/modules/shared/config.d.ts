b048ca3e0aacee9617228277cdac87ababcf359f
eythaann <eythan.cvt@gmail.com>
2023-12-08 14:19:58 -0500

    feature: Updating testing library part 3

--- lib/modules/shared/config.d.ts
+++ lib/modules/shared/config.d.ts
@@ -1,7 +1,11 @@
 import { defaultOnAny, modify } from '../infrastructure';
 
-type DEFAULT_CONFIG = defaultOnAny<typeof import('../../../rt.config').default, never>;
+type DEFAULT_CONFIG = {
+  development: true;
+  conditionWay: 'natural';
+};
+
 // @ts-ignore
-type CLIENT_CONFIG = defaultOnAny<typeof import('../../../../rt.config').default, never>;
+type CLIENT_CONFIG = defaultOnAny<typeof import('../../../../../rt.config').default, never>;
 
 export type CONFIG = modify<DEFAULT_CONFIG, CLIENT_CONFIG>;
